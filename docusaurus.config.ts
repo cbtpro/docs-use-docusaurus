7d0396f0c8dad6192ec1bd7a2212cec7863c8cc1
cbtpro@aliyun.com <bitao.chen@useful-person.com>
2026-08-01 16:53:14 +0800

    chore: 修改baseUrl为根路径
    将网站基础路径从/docs-use-docusaurus/调整为/，适配当前部署路径

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   url: 'https://docs-use-docusaurus.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs-use-docusaurus/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
